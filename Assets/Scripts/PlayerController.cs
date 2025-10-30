774365ac9b9fcac8ec1688c11382a24dce3a9a6f
jspero77 <80274538+jspero77@users.noreply.github.com>
2025-10-30 15:28:26 -0400

    Merge
    Beep beep

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,80 +4,128 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float accelerationTime = 1f;
-    public float decelerationTime = 1f;
-    public float currentSpeed = 0f;
-    public float maxSpeed;
-    float acceleration;
-    public float jumpVelocity;
-    Vector2 velocity;
+    public float accelerationTime = 2f;
+    public float maxSpeed = 5f;
+    private float currentSpeed = 0f;
+    private float acceleration;
+    private Vector2 velocity;
 
-    Rigidbody2D rb;
+    public float jumpVelocity = 7f;
 
-    public LayerMask ground;
     public Transform player;
+    public LayerMask ground;
+
+    private Rigidbody2D rb;
+
+    public SpriteRenderer sprite;
+
+    public FacingDirection currentDirection = FacingDirection.right;
+
+    public enum FacingDirection
+    {
+        left, right
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        acceleration = maxSpeed / accelerationTime;
+
     }
 
     void Update()
     {
-        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
-        MovementUpdate(playerInput);
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
 
-       // Debug.Log(IsGrounded());
+        Debug.Log(IsGrounded());
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        MovementUpdate(playerInput);
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        acceleration = maxSpeed / accelerationTime;
 
-        if (playerInput.magnitude > 0)
+        if (IsTouchingWall())
+        {
+            playerInput.x = 0;
+        }
+
+        if (playerInput.x != 0)
         {
             currentSpeed += acceleration * Time.deltaTime;
-
             if (currentSpeed > maxSpeed)
-            {
                 currentSpeed = maxSpeed;
-            }
-
-            velocity = playerInput * currentSpeed;
         }
-
         else
-
         {
             currentSpeed -= acceleration * Time.deltaTime;
-
             if (currentSpeed < 0)
-            {
                 currentSpeed = 0;
-            }
-
-            velocity = velocity.normalized * currentSpeed;
         }
 
-        transform.position += (Vector3)velocity * Time.deltaTime;
+        rb.velocity = new Vector2(playerInput.x * currentSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-        }
+        GetFacingDirection();
     }
 
-    public bool IsWalking()
+    private void Jump()
     {
-        return currentSpeed > 0;
-
+        rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
     }
-    public bool IsGrounded()
+
+    private bool IsGrounded()
     {
-        bool checkGrounded = Physics2D.Raycast(player.position, Vector2.down, 0.6f, ground);
+        bool grounded = Physics2D.Raycast(player.position, Vector2.down, 0.6f, ground);
         Debug.DrawRay(player.position, Vector2.down * 0.6f, Color.green);
-
-        return checkGrounded;
+        return grounded;
     }
 
+    private bool IsTouchingWall()
+    {
 
+        Vector2 direction;
+
+        if (currentDirection == FacingDirection.right)
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.left;
+        }
+
+        bool touchingWall = Physics2D.Raycast(player.position, direction, 0.55f, ground);
+        Debug.DrawRay(player.position, direction * 0.55f, Color.red);
+
+        return touchingWall;
+
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Debug.Log(horizontalInput);
+
+        if (horizontalInput > 0)
+        {
+            currentDirection = FacingDirection.right;
+            sprite.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            currentDirection = FacingDirection.left;
+            sprite.flipX = true;
+        }
+
+        return currentDirection;
+    }
 }
